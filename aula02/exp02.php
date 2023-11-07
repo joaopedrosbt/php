f6f3b0cbeca6db5862387e231d765bdc463e0914
joao_Pedro <145465734+joaopedrosbt@users.noreply.github.com>
2023-11-07 11:29:30 -0300

    Add files via upload

---
+++ aula02/exp02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+
+    <h1> Meu titulo </h1>
+    <p> Paragrafo </P>
+    <h2> Meu titulo 2 </h2>
+    <p id="p2"> Paragrafo 2 </p>
+
+    <h1 class= estilo> Paragrafo de classe qualquer </h1>
+    <p class= estilo> Paragrafo de classe qualquer </p>
+    <p class= estilo> Paragrafo de classe qualquer </p>
+
+</body>
+</html>
